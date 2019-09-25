7db8722e29844b786bd0c5f117bf191fa2a01e7a
Manik MT <manikhossen0000@gmail.com>
2019-09-25 17:55:13 +0600

    testJs

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function bulb1(){
+	document.getElementById("bulb").src = "img/pic_bulboff.gif";
+}
+function bulb2(){
+	document.getElementById("bulb").src = "img/pic_bulbon.gif";
+}
+
+
+function girls1(){
+	document.getElementById("img").src = "img/grils2.jpg";
+}
+function girls2(){
+	document.getElementById("img").src = "img/grils1.jpg";
+}
+
+function date(){
+	document.getElementById("date").innerHTML = Date();
+}
+
+function D_block(){
+	document.getElementById("display").style.display = "block";
+}
+function D_none(){
+	document.getElementById("display").style.display = "none";
+}
+function fullMeaning(){
+	document.getElementById("text").innerHTML = "Manik Tamzid";
+}
+function font(){
+	document.getElementById("font").style.fontSize = "38px";
+}
